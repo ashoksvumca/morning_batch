36ab79a2d24faf7fddba5773a454cd87afde1d14
AshokSVU <ashoksvumca@gmail.com>
2019-07-05 06:53:05 +0530

    shell script

--- retail.sh
+++ retail.sh
@@ -1,3 +1,6 @@
-This is shell code for our project...
+#! /usr/bin/bash
 
-Please keep it on top
+touch jenkins_file.log
+move jenkins_file.log /home/ec2-user
+cd /home/ec2-user
+chmod 777 jenkins_file.log
